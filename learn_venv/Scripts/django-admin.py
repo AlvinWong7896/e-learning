73d194f5c06d7be037f1456fa53901afbcd598ce
CK Wong <alvinwong7896@gmail.com>
2024-01-28 20:31:07 +0800

    created a versatile data model for e-learning platform

---
+++ learn_venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\alvin\desktop\_repos\e-learning\learn_venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
