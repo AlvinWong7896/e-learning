fc88b3e924d0f54d50bd0e06466f66b38c0f14e1
CK Wong <alvinwong7896@gmail.com>
2024-01-31 23:41:54 +0800

    completed building API by including Requests library to consume it, not tested yet

--- educa/courses/api/serializers.py
+++ educa/courses/api/serializers.py
@@ -1,5 +1,18 @@
 from rest_framework import serializers
-from ..models import Subject, Course, Module
+from ..models import Subject, Course, Module, Content
+
+
+class ItemRelatedField(serializers.RelatedField):
+    def to_representation(self, value):
+        return value.render()
+
+
+class ContentSerializer(serializers.ModelSerializer):
+    item = ItemRelatedField(read_only=True)
+
+    class Meta:
+        model = Content
+        fields = ["order", "item"]
 
 
 class SubjectSerializer(serializers.ModelSerializer):
@@ -27,3 +40,28 @@ class ModuleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Module
         fields = ["order", "title", "description"]
+
+
+class ModuleWithContentsSerializer(serializers.ModelSerializer):
+    contents = ContentSerializer(many=True)
+
+    class Meta:
+        model = Module
+        fields = ["order", "title", "description", "contents"]
+
+
+class CourseWithContentsSerializer(serializers.ModelSerializer):
+    modules = ModuleWithContentsSerializer(many=True)
+
+    class Meta:
+        model = Course
+        fields = [
+            "id",
+            "subject",
+            "title",
+            "slug",
+            "overview",
+            "created",
+            "owner",
+            "modules",
+        ]
